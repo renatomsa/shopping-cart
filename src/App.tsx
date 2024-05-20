ed0fe151039bc39e2fb6355e19b45b789c432ce0
Renato Serrano <renatom.sandrade@gmail.com>
2024-05-19 23:25:38 -0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
 function App() {
   return (
-    <h1>
-      hey
-    </h1>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
